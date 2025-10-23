69ebbaa7665c75a14e9c39544d4d512b2dc2de42
Donya <ftpaknejad@gmail.com>
2025-10-24 00:51:05 +0200

    undo

---
+++ TaskManagerCLI/TaskManagerCLI/build/tests/CMakeFiles/TaskManagerTest.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/niuklear/Portfolio/TaskManagerCLI/TaskManagerCLI/tests/TaskManagerTest.cpp" "tests/CMakeFiles/TaskManagerTest.dir/TaskManagerTest.cpp.o" "gcc" "tests/CMakeFiles/TaskManagerTest.dir/TaskManagerTest.cpp.o.d"
+  "/Users/niuklear/Portfolio/TaskManagerCLI/TaskManagerCLI/src/Task.cpp" "tests/CMakeFiles/TaskManagerTest.dir/__/src/Task.cpp.o" "gcc" "tests/CMakeFiles/TaskManagerTest.dir/__/src/Task.cpp.o.d"
+  "/Users/niuklear/Portfolio/TaskManagerCLI/TaskManagerCLI/src/TaskManager.cpp" "tests/CMakeFiles/TaskManagerTest.dir/__/src/TaskManager.cpp.o" "gcc" "tests/CMakeFiles/TaskManagerTest.dir/__/src/TaskManager.cpp.o.d"
+  "/Users/niuklear/Portfolio/TaskManagerCLI/TaskManagerCLI/src/TaskRepository.cpp" "tests/CMakeFiles/TaskManagerTest.dir/__/src/TaskRepository.cpp.o" "gcc" "tests/CMakeFiles/TaskManagerTest.dir/__/src/TaskRepository.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TaskManagerCLI/TaskManagerCLI/build/tests/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /Users/niuklear/Portfolio/TaskManagerCLI/TaskManagerCLI/tests
+# Build directory: /Users/niuklear/Portfolio/TaskManagerCLI/TaskManagerCLI/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TaskTest "/Users/niuklear/Portfolio/TaskManagerCLI/TaskManagerCLI/build/tests/TaskTest")
+set_tests_properties(TaskTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/niuklear/Portfolio/TaskManagerCLI/TaskManagerCLI/tests/CMakeLists.txt;7;add_test;/Users/niuklear/Portfolio/TaskManagerCLI/TaskManagerCLI/tests/CMakeLists.txt;0;")
+add_test(TaskManagerTest "/Users/niuklear/Portfolio/TaskManagerCLI/TaskManagerCLI/build/tests/TaskManagerTest")
+set_tests_properties(TaskManagerTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/niuklear/Portfolio/TaskManagerCLI/TaskManagerCLI/tests/CMakeLists.txt;17;add_test;/Users/niuklear/Portfolio/TaskManagerCLI/TaskManagerCLI/tests/CMakeLists.txt;0;")
